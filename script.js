3896d3782027f111f8114e82c63b793655358bfc
maqincungmax913-droid <maqincungmax913@gmail.com>
2026-07-13 16:00:29 +0700

    ipdyhnej

--- script.js
+++ script.js
@@ -37,7 +37,9 @@ Aku mungkin tidak pernah tahu bagaimana akhir dari perjalanan kita nanti, tapi a
 Ada hal kecil dari dirimu yang mungkin tidak kamu sadari, tapi mampu membuatku tersenyum dan merasa bahagia.
 
 Aku tahu aku masih jauh dari kata sempurna.Aku punya kekurangan, punya banyak hal yang masih harus aku perbaiki, dan ada bagian dari masa laluku yang membentuk siapa aku sekarang.Tapi satu hal yang ingin aku lakukan adalah terus belajar menjadi seseorang yang pantas untuk berada di sampingmu. 
+Aku tidak ingin hanya menjadi seseorang yang singgah sebentar dalam hidupmu. Aku ingin menjadi bagian dari cerita yang akan selalu kamu ingat, cerita yang penuh dengan tawa, perjuangan, dan kenangan indah yang kita buat bersama.
 
+Mungkin perjalanan kita masih panjang. Mungkin akan ada banyak hal yang harus kita pelajari satu sama lain. Tapi selama kita masih sama-sama memilih untuk berjalan bersama, aku ingin menikmati setiap prosesnya.
 Terima kasih sudah hadir di hidupku. Terima kasih sudah menjadi kamu.
 
 Our story forever with you🤍`;
